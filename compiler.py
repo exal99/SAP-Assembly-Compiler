3d5f7d38f700e64b42bb91a71eba18515ec015d8
Alexander Simko <asimko94@gmail.com>
2017-05-05 12:43:56 +0200

    made compiler output to file

--- compiler.py
+++ compiler.py
@@ -3,18 +3,19 @@ import argparse
 
 
 def get_args():
-	parser = argparse.ArgumentParser(description="A compiler for the SPA (Simple As Posible) computer")
+	parser = argparse.ArgumentParser(description="A compiler for the SAP (Simple As Posible) computer")
 
 	parser.add_argument("file", type=argparse.FileType("r"), help="The program file to compile")
+	parser.add_argument("-o", "--out", default="a.sap", help="The output file name", metavar="<file name>")
 
 	return parser.parse_args()
 
 
 def main():
-	program = get_args().file.read()
+	args = get_args()
+	program = args.file.read()
 	compiled = asm_parser.parse(program)
-	print()
-	print(compiled)
+	print(compiled, file=open(args.out, "w"), end="")
 
 if __name__ == '__main__':
 	main()
